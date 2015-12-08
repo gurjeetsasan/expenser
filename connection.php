871edeb71baa304af4717bb7b1573d29aa3f9b47
Gurjeet Singh <gurjeet.sasan@gmail.com>
2015-12-08 18:02:55 +0530

    update Daily functionality

--- connection.php
+++ connection.php
@@ -87,6 +87,36 @@ class  Expenser{
 
 	/*fucntion to add Dailies */
 	function addDailies( $data = null ){
+ 
+		$date  			= 	$data['date'];
+		$amount 		=	$data['amount'];
+		$notes			=	$data['notes'];
+		$attachment		=	$data['attachment'];
+		$category_id	=	$data['category_id'];	
+		$user_id		=	$data['user_id'];
+
+		
+		$sql = "INSERT INTO `dailies`( `date`, `amount`, `notes`, `attachment`, `category_id`, `user_id`) 
+					VALUES ( '".$date."', '".$amount."','".$notes."' , '".$attachment."', '".$category_id."', '".$user_id."' );";
+		
+		$result = mysqli_query( $this->connection , $sql);
+
+		$resultArray 	= array();
+		$dataArray 		= array();
+
+		$resultArray 	= array();					
+		if ( $this->connection->query($sql) === TRUE) {
+		    $resultArray['status'] 	= 'success';
+			$resultArray['message'] = "New Daily created successfully";
+		}else{
+			$resultArray['status'] 	= 'error';
+			$resultArray['message'] = "Error, DAily not created.";
+		}
+
+	
+
+		/*reuturn result*/
+		echo json_encode($resultArray);
 		
 	}
 
@@ -122,8 +152,21 @@ class  Expenser{
 
 
 $expenser = new Expenser;
-$data['categoryname']	=	'test-category';
-$data['description']	=	'test-category Descriptions';
-$data['user_id']		=	'1';
 
-$expenser->addCategory($data);
+$data['date']			=  	'08-12-2015';
+$data['amount']			= 	'100';
+$data['notes']			= 	'notes';
+$data['attachment']		= 	'attachment';
+$data['category_id']	= 	'1';	
+$data['user_id']		= 	'1';
+
+
+$expenser->addDailies( $data );
+
+
+// $expenser = new Expenser;
+// $data['categoryname']	=	'test-category';
+// $data['description']	=	'test-category Descriptions';
+// $data['user_id']		=	'1';
+
+// $expenser->addCategory($data);
